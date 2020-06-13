75b398360a045268b1211471a5aa2f0f564201cf
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-13 16:43:34 +0100

    Add port variable

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
-app.listen(3000, () => {
-  console.log("Listening to port 3000");
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 });
