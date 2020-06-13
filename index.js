58d5360220aa1dccb5850aa725dd045d41f5013d
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-13 16:53:29 +0100

    Amend typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
